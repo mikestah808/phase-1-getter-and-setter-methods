010e316efbfb6e56455550246b2cf710e59860bd
Michael Stafford <michaelstafford808@gmail.com>
2022-02-12 16:43:25 -1000

    commit tree c1ee383f576a180ffc41c231e8e730cd9fd7b487

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your Circle class here
+
+
+class Circle{
+
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return this.radius * 2;
+    }
+
+    set diameter(newDiameter){
+        return this.radius = newDiameter / 2;
+    }
+
+
+    get circumference(){
+        return Math.PI * this.diameter;
+    }
+
+    set circumference(newCircumference){
+        this.radius = newCircumference / (2 * Math.PI)
+    }
+
+    get area(){
+        return Math.PI * this.radius**2
+    }
+
+    set area(newArea){
+        this.radius = Math.sqrt(newArea/Math.PI);
+    }
+
+
+
+}
+
+
+//getters => access properties
+//setters => change (mutate) them
+
+
